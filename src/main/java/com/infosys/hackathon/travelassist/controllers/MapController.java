62d463eb9484fa48e41986007053a2345f5cc0df
Ayan Chakraborty <ayan.chakraborty@outlook.com>
2016-03-05 01:16:15 +0530

    map controller.

---
+++ src/main/java/com/infosys/hackathon/travelassist/controllers/MapController.java
@@ -0,0 +1,46 @@
+package com.infosys.hackathon.travelassist.controllers;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.infosys.hackathon.services.ResultCodes;
+import com.infosys.hackathon.services.country.dto.CountryServiceResponse;
+import com.infosys.hackathon.services.exceptions.TravelServiceException;
+import com.infosys.hackathon.travelassist.services.CountryServiceClient;
+import com.infosys.hackathon.travelassist.utils.ApplicationConstants;
+
+@Controller
+public class MapController {
+
+	private static final String COUNTRY_ALLCITIES_HANDLER = "allcities";
+
+	private static final Logger LOGGER = Logger.getLogger(MapController.class);
+
+	@Autowired
+	private CountryServiceClient countryServiceClient;
+
+	@RequestMapping(value = "/map", method = RequestMethod.GET)
+	public String showFilters(@CookieValue("coun") String country,
+		Model model) {
+		LOGGER.info("fetching states of specific country");
+		CountryServiceResponse serviceResponse = new CountryServiceResponse();
+		try {
+			serviceResponse = countryServiceClient.getRequest(
+				COUNTRY_ALLCITIES_HANDLER, CountryServiceResponse.class,
+				country);
+			LOGGER.info("successfully processed, "
+				+ serviceResponse.getDetails().size() + " cities returned");
+
+			model.addAttribute("cities", serviceResponse.getDetails());
+		} catch (TravelServiceException e) {
+			serviceResponse.setResult(ResultCodes.Failure);
+		}
+
+		return ApplicationConstants.MAP_PAGE;
+	}
+}
--- src/main/java/com/infosys/hackathon/travelassist/utils/ApplicationConstants.java
+++ src/main/java/com/infosys/hackathon/travelassist/utils/ApplicationConstants.java
@@ -6,6 +6,7 @@ public class ApplicationConstants {
 	public static final String HELP_PAGE = "help";
 	public static final String HOME_PAGE = "home";
 	public static final String EMPLOYEE_LISTING_PAGE = "employee-listing";
+	public static final String MAP_PAGE = "map";
 	public static final String CONTACT_PAGE = "contactUs";
 	public static final String EMERGENCY_CONTACT_PAGE = "emergency-contact";
 
