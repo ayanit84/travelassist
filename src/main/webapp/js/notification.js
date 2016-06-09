3554e6e0e448707ed5e664515c888be8821481a0
saikamini <saikamini.9@gmail.com>
2016-06-09 17:38:20 +0530

    Push notifications css changes

--- src/main/webapp/js/notification.js
+++ src/main/webapp/js/notification.js
@@ -6,7 +6,7 @@
 		
 		// create the notification
 		var notification = new NotificationFx({
-			message : '<p>Your cab has been booked.<a href="#">view more</a>.</p>',
+			message : '<p>Your cab has been booked.<br/> CAB- OD 02X81111 INDIGO,<br/> DRV- SONU 7873699142</p>',
 			layout : 'growl',
 			effect : 'scale',
 			type : 'notice', // notice, warning, error or success
